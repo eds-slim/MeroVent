e49e3ed495d8a534af0aff4f37423467ce682de0
ES <>
2021-09-19 15:28:49 +0200

    interpolation

--- R/explore.r
+++ R/explore.r
@@ -16,11 +16,6 @@ dd %>% group_by(ID, TIME, CKDEPI, LOGPRO) %>%
 require(Hmisc)
 Hmisc::describe(dd)
 
-dd %>% group_by(TIME, CKDEPI, LOGPRO) %>% 
-  summarise(meanc = mean(C_VENT)) %>% 
-  filter(TIME == 96) %>% 
-  ggplot(aes(x = CKDEPI, y = LOGPRO, fill = meanc)) +
-  geom_tile()
 
 
 dd %>% group_by(TIME, CKDEPI, LOGPRO) %>% 
@@ -41,45 +36,14 @@ dd %>% group_by(TIME, CKDEPI, LOGPRO) %>%
   geom_point()
 
 
-dd %>% group_by(ID, CKDEPI, LOGPRO, dose) %>% 
+
+
+
+
+temp <- d.plot.2 %>% select(CKDEPI, LOGPRO, dose, p) %>%  
+  group_by(CKDEPI, LOGPRO, dose) %>% 
   #filter(TIME==96) %>% 
-  summarise(p = sum(C_VENT>=8)/n()) %>% 
-  ggplot(aes(x = CKDEPI, y = LOGPRO, fill = p)) +
-  geom_tile() +
-  facet_wrap(~dose) +
-  scale_fill_gradient2(low = 'red', mid = 'yellow', high = 'darkgreen', midpoint = .5)
-
-d.plot1 <- dd %>% group_by(ID, CKDEPI, LOGPRO, dose) %>% 
-  #filter(TIME==96) %>% 
-  filter(CKDEPI %in% c(50,60,70) & LOGPRO %in% c(2.3, 2.4, 2.6)) %>% 
-  summarise(p = sum(C_VENT>=2)/n()) %>% 
-  ungroup() %>% group_by(CKDEPI, LOGPRO) %>% nest() %>% 
-  mutate(tps = map(data, ~fields::Tps(.$dose, .$p, lambda = 0))
-         , tps.r = map(tps, ~.$fitted.values)) 
-
-d.plot.2 <- d.plot1 %>% 
-  mutate(tps.x = map(tps, ~seq(0,6,.1))
-         , tps.y = map(tps, ~predict(., seq(0,6,.1))[,1])) %>% 
-  unnest(c(tps.x, tps.y))
-
-d.plot1 %>% 
-  unnest(c(tps.r, data)) %>% 
-  ggplot(aes(x = dose, y = p)) +
-  geom_point() +
-  geom_point(aes(y = `tps.r`)) +
-  geom_line(data = d.plot.2, inherit.aes = FALSE, aes(x=tps.x, y=tps.y), color = 'orange') +
-  ggalt::geom_xspline(spline_shape = -0.5, color = 'darkgreen') +
-  facet_grid(CKDEPI ~ LOGPRO, scales = 'free')
-
-tps <- dd %>% group_by(ID, CKDEPI, LOGPRO, dose) %>% 
-  #filter(TIME==96) %>% 
-  filter(CKDEPI %in% c(60) & LOGPRO %in% c(2.4)) %>% 
-  summarise(p = sum(C_VENT>=2)/n()) %$% 
-  fields::Tps(.$dose, .$p, lambda = 0)
-
-temp <- dd %>% group_by(ID, CKDEPI, LOGPRO, dose) %>% 
-  #filter(TIME==96) %>% 
-  summarise(p = sum(C_VENT>=2)/n()) %>% 
+  #summarise(p = sum(C_VENT>=2)/n()) %>% 
   ungroup() %>% group_by(dose)
 
 temp %>% 
@@ -106,16 +70,10 @@ temp %>% nest() %>%
     unnest(c(tps.xy, tps.z)) %>% 
   ungroup() %>% group_by(x, y) %>% 
   filter(tps.z>=0.9) %>% slice(which.min(dose)) %>% 
-  ggplot(aes(x = x, y = y, fill = as.factor(dose))) +
+  ggplot(aes(x = x, y = y, fill = dose)) +
   geom_tile()
   
-dd %>% group_by(ID, CKDEPI, LOGPRO, dose) %>% 
-  filter(TIME==96) %>% 
-  summarise(p = sum(C_VENT>=2)/n()) %>% 
-  ungroup() %>% group_by(CKDEPI, LOGPRO) %>% 
-  filter(p>=0.9) %>% slice(which.min(dose)) %>% 
-  ggplot(aes(x = CKDEPI, y = LOGPRO, fill = as.factor(dose))) +
-  geom_tile()
+
 
 
 dd %>% group_by(ID, CKDEPI, LOGPRO, dose) %>% 
